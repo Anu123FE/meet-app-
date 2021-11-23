6d8e271c327df4214da051d3bae6abec5deda68c
Anu123FE <anupama.nair3006@gmail.com>
2021-11-22 22:23:15 -0500

    updated changes

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -17,7 +17,6 @@ class CitySearch extends Component {
       handleInputChanged = (event) => {
         const value = event.target.value;
         const suggestions = this.props.locations.filter((location) => {
-          console.log(location)
             return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
           });
           this.setState({
@@ -25,7 +24,6 @@ class CitySearch extends Component {
             suggestions,
            
           });
-          console.log(this.state.event)
         };
         handleItemClicked = (suggestion) => {
           this.setState({
@@ -55,7 +53,7 @@ class CitySearch extends Component {
             </li>
            </ul>
            <br/><br/>
-              <NumberOfEvents  updateNumberOfEvents = {this.updateNumberOfEvents} />
+              <NumberOfEvents  updateNumberOfEvents = {this.updateNumberOfEvents} /> <font size="1.5"><i>Type a number and press enter</i></font>
            <hr width="100%"></hr>
            <br/>
            <EventList events={this.state.events} />
