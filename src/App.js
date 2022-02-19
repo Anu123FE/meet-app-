bf4edfa5d25a542354b821e7d1b4a74f81dc692e
Anu123FE <anupama.nair3006@gmail.com>
2022-02-19 13:45:58 -0500

    Recharts

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import './App.css';
 import CitySearch from './CitySearch';
 import { mockData } from './mock-data';
 import { extractLocations } from './api';
+import WelcomeScreen from './WelcomeScreen';
+import { getEvents, checkToken, getAccessToken } from './api';
 
 class App extends Component {
   state = {
-    locations : extractLocations(mockData)
+    locations : extractLocations(mockData),
+    showWelcomeScreen: undefined
   }
   componentDidMount()
   {
     localStorage.setItem('events', JSON.stringify(mockData))
   }
   render() {
+    
     return (
       <div className="App">
         <CitySearch locations={this.state.locations}/>
--- src/EventList.js
+++ src/EventList.js
@@ -1,17 +1,65 @@
 import React, { Component } from 'react';
+import { ResponsiveContainer, ScatterChart, CartesianGrid, XAxis, YAxis, Tooltip, Scatter } from 'recharts';
 import Event from './Event';
+import { mockData } from './mock-data';
+
+const data = [
+  { x: 100, y: 200, z: 200 },
+  { x: 120, y: 100, z: 260 },
+  { x: 170, y: 300, z: 400 },
+  { x: 140, y: 250, z: 280 },
+  { x: 150, y: 400, z: 500 },
+  { x: 110, y: 280, z: 200 },
+];
+
+console.log(mockData)
+const locations = mockData.map(x=>x.location);
+//  const evts = mockData.filter( data  => data.location === suggestion);
+const dt = locations.map(x=> {
+  const n = mockData.filter(y=>y.location === x).length;
+  return {location: x, event: n}
+});
+
+console.log(dt)
+// const dt = mockData.map(x=> 
+//   [
+//     x.
+//   ])
 
 class EventList extends Component {
     render() {
         const { events } = this.props;
         return (
-          <ul className="EventList">
+          <div>
+             <h1>eufduhfuogg</h1>
+             
+             <ScatterChart
+          width={800}
+          height={400}
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20,
+          }}
+        >
+          <CartesianGrid />
+          <XAxis type="category" dataKey="location" name="stature" />
+          <YAxis type="events" dataKey="event" name="events" />
+          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+          <Scatter name="A school" data={dt} fill="#8884d8" />
+        </ScatterChart>
+
+        <ul className="EventList">
             {events.map(event =>
               <li key={event.id}>
                 <Event e = {event} />
               </li>
             )}
           </ul>
+
+          </div>
+         
         );
     }
 }
